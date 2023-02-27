387a2a2c0845295101ca2ce9757fca31fad0bf6c
Ali Zahid <ali.zahid@live.com>
2023-02-27 05:59:04 +0400

    Seeder updated

--- apps/web/prisma/seed.ts
+++ apps/web/prisma/seed.ts
@@ -1,10 +1,12 @@
 import { faker } from '@faker-js/faker'
 import { PrismaClient } from '@prisma/client'
+import { addMinutes, subMinutes } from 'date-fns'
 import { range, sample } from 'lodash-es'
 
 const prisma = new PrismaClient()
 
 const main = async () => {
+  const aliEmail = 'ali.zahid@live.com'
   const buildingId = '2423786279'
 
   const building = await prisma.building.upsert({
@@ -20,13 +22,40 @@ const main = async () => {
     },
   })
 
+  await prisma.comment.deleteMany({})
+  await prisma.post.deleteMany({})
+  await prisma.resident.deleteMany({})
+
+  await prisma.user.deleteMany({
+    where: {
+      email: {
+        not: aliEmail,
+      },
+    },
+  })
+
+  await prisma.resident.create({
+    data: {
+      building: {
+        connect: {
+          id: buildingId,
+        },
+      },
+      user: {
+        connect: {
+          email: aliEmail,
+        },
+      },
+    },
+  })
+
   const users = range(200).map(() => faker.datatype.uuid())
 
   await Promise.all(
     users.map((id) =>
       prisma.user.create({
         data: {
-          email: faker.internet.email(),
+          email: faker.internet.email().toLowerCase(),
           id,
           image: faker.image.people(800, 800, true),
           name: `${faker.name.firstName()} ${faker.name.lastName()}`,
@@ -44,28 +73,32 @@ const main = async () => {
     data: range(1_000).map(() => ({
       attachments:
         faker.datatype.number(100) > 90
-          ? range(faker.datatype.number(5)).map(() =>
-              faker.image.food(800, 600, true)
-            )
+          ? range(faker.datatype.number(5)).map(() => ({
+              type: 'image',
+              url: faker.image.food(800, 600, true),
+            }))
           : [],
       body: faker.lorem.paragraph(),
       buildingId,
-      userId: sample(users) as string,
+      createdAt: subMinutes(new Date(), faker.datatype.number(100_000)),
+      userId: String(sample(users)),
     })),
   })
 
   const posts = await prisma.post.findMany({
     select: {
+      createdAt: true,
       id: true,
     },
   })
 
   await prisma.comment.createMany({
-    data: posts.flatMap(() =>
+    data: posts.flatMap(({ createdAt, id }) =>
       range(faker.datatype.number(5)).map(() => ({
         body: faker.lorem.sentence(),
-        postId: sample(posts)?.id as string,
-        userId: sample(users) as string,
+        createdAt: addMinutes(createdAt, faker.datatype.number(10_000)),
+        postId: id,
+        userId: String(sample(users)),
       }))
     ),
   })

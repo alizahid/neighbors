6887e8151c8f671108a2cc1f123b7b1b0b57f408
Ali Zahid <ali.zahid@live.com>
2023-03-18 02:42:46 +0400

    Sentry added

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,22 @@
+const { withSentryConfig } = require('@sentry/nextjs')
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+const config = {
   env: {
+    SENTRY_DSN: process.env.SENTRY_DSN,
     SUPABASE_PUBLIC_KEY: process.env.SUPABASE_PUBLIC_KEY,
     SUPABASE_URL: process.env.SUPABASE_URL,
   },
   reactStrictMode: true,
   transpilePackages: ['shared'],
 }
+
+module.exports = withSentryConfig(
+  config,
+  {
+    silent: true,
+  },
+  {
+    hideSourcemaps: true,
+  }
+)
